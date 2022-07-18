5c1600a1e25dddcc5b759bf149dc75fd142d3c65
Frank Kelly <65137387+frank-kelly@users.noreply.github.com>
2022-07-18 05:35:13 +0100

    Added componenets, styles and layout

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test edit to confirm git setup</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter Goal Here..." />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    // height: 40,
+    marginRight: 20,
+    paddingLeft: 10,
+    paddingTop: 6,
+    paddingBottom: 6,
+    width: "70%",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
